1ec1f8a4ab41b9c069c811a2712a91cc9066912b
kojurohan42 <kojurohan42@gmail.com>
2022-07-06 22:07:49 +0545

    day4 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,4 @@
-import 'dart:ui';
+// ignore_for_file: prefer_const_constructors
 
 import "package:flutter/material.dart";
 
@@ -7,14 +7,62 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text("Login Page",
+    return Material(
+      color: Colors.white,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 40.0,
+          ),
+          Image.asset(
+            "assets/images/login_image.png",
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text(
+            "Welcome",
             style: TextStyle(
-              fontSize: 20,
-              color: Colors.blue,
+              fontSize: 22,
               fontWeight: FontWeight.bold,
-            )),
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Username",
+                    labelText: "Username",
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password",
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                ElevatedButton(
+                  child: Text("Login"),
+                  style: TextButton.styleFrom(),
+                  onPressed: () {
+                    print("login boy");
+                  },
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
