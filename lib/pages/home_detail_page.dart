dd26435e02a39fb6c4f913e2c62a2d178217baa0
kojurohan42 <kojurohan42@gmail.com>
2022-07-14 09:31:47 +0545

    day18 completed

---
+++ lib/pages/home_detail_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_catalog/widgets/themes.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+import '../models/catalog.dart';
+
+class HomeDetailPage extends StatelessWidget {
+  const HomeDetailPage({super.key, required this.catalog});
+  final Item catalog;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      backgroundColor: MyTheme.creamColor,
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        child: ButtonBar(
+          alignment: MainAxisAlignment.spaceBetween,
+          buttonPadding: EdgeInsets.zero,
+          children: [
+            "\$${catalog.price}".text.bold.xl4.red800.make(),
+            ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(MyTheme.darkBluishColor),
+                        shape: MaterialStateProperty.all(
+                          StadiumBorder(),
+                        )),
+                    child: "Buy".text.make())
+                .wh(100, 50),
+          ],
+        ).p32(),
+      ),
+      body: SafeArea(
+          child: Column(
+        children: [
+          Hero(
+                  tag: Key(catalog.id.toString()),
+                  child: Image.network(catalog.image))
+              .h32(context),
+          Expanded(
+              child: VxArc(
+            height: 30.0,
+            edge: VxEdge.TOP,
+            arcType: VxArcType.CONVEY,
+            child: Container(
+              color: Colors.white,
+              width: context.screenWidth,
+              child: Column(
+                children: [
+                  catalog.name.text.xl4
+                      .color(MyTheme.darkBluishColor)
+                      .bold
+                      .make(),
+                  catalog.desc.text.textStyle(context.captionStyle).xl.make(),
+                  10.heightBox,
+                ],
+              ).py64(),
+            ),
+          ))
+        ],
+      )),
+    );
+  }
+}
