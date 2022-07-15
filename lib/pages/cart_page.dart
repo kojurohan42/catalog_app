7332b25a0d34adae491be65b10af106a98b906de
kojurohan42 <kojurohan42@gmail.com>
2022-07-15 08:23:01 +0545

    day22 completed

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -8,10 +8,66 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: context.canvasColor,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: "Cart".text.xl.make(),
+        backgroundColor: context.canvasColor,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: "Cart".text.xl.make(),
+        ),
+        body: Column(
+          children: [
+            _CartList().p32().expand(),
+            Divider(),
+            _CartTotal(),
+          ],
+        ));
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  const _CartTotal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\$99".text.xl5.color(context.theme.accentColor).make(),
+          30.widthBox,
+          ElevatedButton(
+            onPressed: () {},
+            style: ButtonStyle(
+              backgroundColor:
+                  MaterialStateProperty.all(context.theme.buttonColor),
+            ),
+            child: "Buy".text.white.make(),
+          ).w32(context)
+        ],
+      ),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  const _CartList({super.key});
+
+  @override
+  State<_CartList> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(Icons.done),
+        trailing: IconButton(
+          icon: Icon(Icons.remove_circle_outline),
+          onPressed: () {},
+        ),
+        title: "Item 1".text.make(),
       ),
     );
   }
