9239b03810cd941c4cfe276cae94b1d57c32e4c4
kojurohan42 <kojurohan42@gmail.com>
2022-07-18 05:56:20 +0545

    day24 completed

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -2,6 +2,11 @@
 import 'package:flutter_catalog/models/catalog.dart';
 
 class CartModel {
+  static final cartModel = CartModel._internal();
+  CartModel._internal();
+
+  factory CartModel() => cartModel;
+  
   late CatalogModel _catalog;
 
   final List<int> _itemIds = [];
